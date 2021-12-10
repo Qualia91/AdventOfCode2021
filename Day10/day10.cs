f47fc9da14aec6fbdc47ef04488a82e3d56cece6
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2021-12-10 21:10:36 +0000

    day 10 done

--- Day10/day10.cs
+++ Day10/day10.cs
@@ -8,13 +8,16 @@ namespace Day10 {
 
         public const string OPEN_CHARS = "([{<";
         public const string CLOSE_CHARS = ")]}>";
-        public Dictionary<char, int> valueDict = new Dictionary<char, int>();
+        public Dictionary<char, long> valueDict = new Dictionary<char, long>();
+        public Dictionary<char, long> valueDictPartTwo = new Dictionary<char, long>();
         public Dictionary<char, char> closeMapping = new Dictionary<char, char>();
     
         static void Main(string[] args)
         {
             Day10 day10 = new Day10();
-            Console.WriteLine("Part One: " + day10.PartOne());
+            Tuple<long, long> retTup = day10.Calculate();
+            Console.WriteLine("Part One: " + retTup.Item1);
+            Console.WriteLine("Part Two: " + retTup.Item2);
         }
 
         public Day10() {
@@ -26,17 +29,25 @@ namespace Day10 {
             closeMapping.Add('[', ']');
             closeMapping.Add('{', '}');
             closeMapping.Add('<', '>');
+            valueDictPartTwo.Add('(', 1);
+            valueDictPartTwo.Add('[', 2);
+            valueDictPartTwo.Add('{', 3);
+            valueDictPartTwo.Add('<', 4);
         }
 
-        public int PartOne() {
+        public Tuple<long, long> Calculate() {
             string[] lines = System.IO.File.ReadAllLines(@"input.txt");
 
-            Dictionary<char, int> errorCounter = new Dictionary<char, int>();
+            Dictionary<char, long> errorCounter = new Dictionary<char, long>();
+
+            List<long> partTwoLineResults = new List<long>();
 
             foreach (string line in lines)
             {
                 
                 Stack<char> openStack = new Stack<char>();
+
+                bool failed = false;
 
                 foreach (char c in line)
                 {
@@ -45,17 +56,22 @@ namespace Day10 {
                     } else if (CLOSE_CHARS.Contains(c)) {
                         var next = closeMapping[openStack.Pop()];
                         if (next != c) {
+                            failed = true;
                             errorCounter = AddToDict(c, errorCounter);
                         }
                     }
                 
                 }
+
+                if (!failed) {
+                    partTwoLineResults.Add(CalculatePartTwoResult(openStack));
+                }
             }
                 
-            return CalcErrors(errorCounter);
+            return new Tuple<long, long>(CalcErrors(errorCounter), Average(partTwoLineResults));
         }
 
-        private Dictionary<char, int> AddToDict(char character, Dictionary<char, int> errorCounter) {
+        private Dictionary<char, long> AddToDict(char character, Dictionary<char, long> errorCounter) {
             if (errorCounter.ContainsKey(character)) {
                 errorCounter[character] = errorCounter[character] + 1;
             } else {
@@ -64,13 +80,26 @@ namespace Day10 {
             return errorCounter;
         }
 
-        private int CalcErrors(Dictionary<char, int> errorCounter) {
-            int sum = 0;
-            foreach(KeyValuePair<char, int> kvp in errorCounter ) {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        private long CalcErrors(Dictionary<char, long> errorCounter) {
+            long sum = 0;
+            foreach(KeyValuePair<char, long> kvp in errorCounter ) {
                 sum += (valueDict[kvp.Key] * kvp.Value);
             }
             return sum;
+        }
+
+        private long CalculatePartTwoResult(Stack<char> openStack) {
+            long sum = 0;
+            foreach(char nextCharacter in openStack) {
+                sum *= 5;
+                sum += valueDictPartTwo[nextCharacter];
+            }
+            return sum;
+        }
+
+        private long Average(List<long> partTwoLineResults) {
+            partTwoLineResults.Sort();
+            return partTwoLineResults[partTwoLineResults.Count / 2];
         }
     }
 }
