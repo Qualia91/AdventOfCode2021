3b2b077cd2ae6b51cd318169abb765e4ac951700
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2021-12-11 17:26:32 +0000

    day 3 refactoring for video

--- Day3/main.js
+++ Day3/main.js
@@ -7,54 +7,45 @@ var lines = fileData.split('\n');
 var binaryDigits = lines[0].length - 1;
 
 var counters = [];
-var bitValues = [];
-var currentBitCounter = 1;
 for (let index = 0; index < binaryDigits; index++) {
-    counters.push([0, 0]);
-    bitValues.push(currentBitCounter);
-    currentBitCounter *= 2;
+    counters[index] = 0;
 }
-bitValues.reverse();
 
 lines.forEach(line => {
     for (let index = 0; index < binaryDigits; index++) {
         if (line.charAt(index) === "0") {
-            counters[index][0] += 1;
+            counters[index] -= 1;
         } else {
-            counters[index][1] += 1;
+            counters[index] += 1;
         }
     }
 });
 
-// Now work out most and least common
+// Now work out the most and least common, and the value
 var sumMost = 0;
 var sumLeast = 0;
-for (let index = 0; index < binaryDigits; index++) {
-
-    if (counters[index][0] <= counters[index][1]) {
-        sumMost += bitValues[index]  
+var bitVal = 1;
+for (let index = binaryDigits - 1; index >= 0; index--) {
+    if (counters[index] > 0) {
+        sumMost += bitVal;
     } else {
-        sumLeast += bitValues[index]  
+        sumLeast += bitVal;
     }
+    bitVal *= 2;
 }
 
-// Part 1 answers
-console.log("Most Sum: " + sumMost);
-console.log("Least Sum: " + sumLeast);
-console.log("Answer: " + sumMost * sumLeast);
+// Part 1 Answer
+console.log("Part One Answer: " + (sumMost * sumLeast));
 
-// Part 2 -
-//  iterate over binary digits
-//  iterate over lines (Oxygen and Co2)
-//  get most and least common
-//  update lines list with only survivors of checks
 
+// Part 2
 
 var oxygenList = lines.slice();
 var co2List = lines.slice();
+
 for (let index = 0; index < binaryDigits; index++) {
 
-    // Get most and least common values of this binary digit for both lists
+    // get the most and least common values for both lists
     if (oxygenList.length > 1) {
         var oxySwing = 0;
         oxygenList.forEach(line => {
@@ -64,7 +55,8 @@ for (let index = 0; index < binaryDigits; index++) {
                 oxySwing++;
             }
         });
-        oxygenList = oxygenList.filter((oxyElem) => {
+
+        oxygenList = oxygenList.filter(oxyElem => {
             if (oxySwing < 0) {
                 return oxyElem.charAt(index) === "0";
             }
@@ -73,7 +65,7 @@ for (let index = 0; index < binaryDigits; index++) {
             }
         });
     }
-    
+
     if (co2List.length > 1) {
         var co2Swing = 0;
         co2List.forEach(line => {
@@ -83,7 +75,8 @@ for (let index = 0; index < binaryDigits; index++) {
                 co2Swing++;
             }
         });
-        co2List = co2List.filter((co2Elem) => {
+
+        co2List = co2List.filter(co2Elem => {
             if (co2Swing < 0) {
                 return co2Elem.charAt(index) === "1";
             }
@@ -92,30 +85,21 @@ for (let index = 0; index < binaryDigits; index++) {
             }
         });
     }
-
 }
 
-// convert to decimal
-function convertBinaryToDecimal(inputBinary) {
-    var index = inputBinary.length;
+// to decimal conversion
+function convertToDecimal(input) {
+    var index = input.length;
     var sum = 0;
     var currentBinDig = 1;
     while (index--) {
-        if (inputBinary.charAt(index) === "1") {
+        if (input.charAt(index) === "1") {
             sum += currentBinDig;
-        }
+        };
         currentBinDig *= 2;
     }
     return sum;
-}
+};
 
-console.log("Part 2");
-var binOxy = oxygenList[0].trim();
-var binCo2 = co2List[0].trim();
-var decOxy = convertBinaryToDecimal(binOxy);
-var decCo2 = convertBinaryToDecimal(binCo2);
-console.log("Oxygen binary: " + binOxy);
-console.log("Oxygen Decimal: " + decOxy);
-console.log("Co2 binary: " + binCo2);
-console.log("Co2 Decimal: " + decOxy);
-console.log("Combination: " + (decOxy * decCo2));
+// Part 2 Answer
+console.log("Part Two Answer: " + (convertToDecimal(oxygenList[0].trim()) * convertToDecimal(co2List[0].trim())));
