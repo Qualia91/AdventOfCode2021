ffc4786172169f5e69710ec64026d45f38e3e9c8
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2021-12-20 09:13:51 +0000

    day 6 refactoring for video

--- Day6/day6.erl
+++ Day6/day6.erl
@@ -16,7 +16,7 @@
 
 -define(SERVER, ?MODULE).
 
--export([both/0, part_one/0, part_two/0]).
+-export([both/0, part_one/0]).
 
 -define(COMMA, 44).
 
@@ -28,6 +28,10 @@ both() ->
     part_one(),
     part_two().
 
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
 part_one() ->
     {ok, BinData} = file:read_file("input.txt"),
     Return = iterate_day(convert_to_int_list(BinData), 80),
@@ -37,16 +41,12 @@ part_two() ->
     {ok, BinData} = file:read_file("input.txt"),
     GrowQueue = convert_to_int_queue(BinData),
     BirthQueue = queue:in(0, queue:in(0, queue:new())),
-    
+
     FinalQueue = iterate_day(BirthQueue, GrowQueue, 256),
     Sum = lists:foldl(fun(Val, SumAcc) ->
         SumAcc + Val
     end, 0, queue:to_list(FinalQueue)),
     io:format("Part 2 Count: ~p~n", [Sum]).
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
 
 convert_to_int_queue(BinData) ->
     Dict = convert_to_int_dict(lists:sort(binary_to_list(BinData)), dict:new()),
@@ -59,22 +59,22 @@ convert_to_int_queue(BinData) ->
             end
         end,
         queue:new(),
-        lists:seq(0, 6)).
+        lists:seq(0,6)).
 
 convert_to_int_dict([], Dict) ->
     Dict;
 convert_to_int_dict([Hd | Tail], Dict) ->
-    convert_to_int_dict(Tail, case Hd of
+    convert_to_int_dict(Tail,
+    case Hd of
         ?COMMA -> Dict;
-        Other -> 
+        Other ->
             NewNumber = Other - 48,
             case dict:find(NewNumber, Dict) of
-                {ok, Val} ->
-                    dict:store(NewNumber, Val + 1, Dict);
-                error ->
-                    dict:store(NewNumber, 1, Dict)
+                {ok, Val} -> dict:store(NewNumber, Val + 1, Dict);
+                error -> dict:store(NewNumber, 1, Dict)
             end
-    end).
+        end).
+
 
 
 convert_to_int_list(BinData) ->
@@ -83,26 +83,11 @@ convert_to_int_list(BinData) ->
 convert_to_int_list([], Acc) ->
     Acc;
 convert_to_int_list([Hd | Tail], Acc) ->
-    convert_to_int_list(Tail, case Hd of
+    convert_to_int_list(Tail,
+    case Hd of
         ?COMMA -> Acc;
-        Other -> 
-            [(Other - 48) | Acc]
+        Other -> [(Other - 48) | Acc]
     end).
-
-iterate_over_binary([], Acc) ->
-    Acc;
-iterate_over_binary([Hd | Tail], Acc) ->
-    UpdatedAcc = case Hd of
-        ?COMMA -> Acc;
-        Other -> 
-            case Other of
-                0 -> [8 | [6 | Acc]];
-                NotZero -> 
-                    Number = NotZero - 1,
-                    [Number | Acc]
-            end
-    end,
-    iterate_over_binary(Tail, UpdatedAcc).
 
 iterate_day(List, 0) ->
     List;
@@ -110,8 +95,19 @@ iterate_day(List, Days) ->
     UpdatedList = iterate_over_binary(List, []),
     iterate_day(UpdatedList, Days - 1).
 
+iterate_over_binary([], Acc) ->
+    Acc;
+iterate_over_binary([Hd | Tail], Acc) ->
+    UpdatedAcc = case Hd of
+        0 -> [8 | [6 | Acc]];
+        NotZero ->
+            Number = NotZero - 1,
+            [Number | Acc]
+    end,
+    iterate_over_binary(Tail, UpdatedAcc).
+
 iterate_day(BirthQueue, GrowQueue, 0) ->
-    merge_queues(queue:out(BirthQueue), GrowQueue);
+    merege_queues(queue:out(BirthQueue), GrowQueue);
 iterate_day(BirthQueue, GrowQueue, Days) ->
     {BirthHead, UpdatedBirthQueue} = queue:out(BirthQueue),
     {GrowHead, UpdatedGrowQueue} = queue:out(GrowQueue),
@@ -126,10 +122,10 @@ iterate_day(BirthQueue, GrowQueue, Days) ->
         _ -> 0
     end,
 
-    iterate_day(queue:in(AddToBirthQueue, UpdatedBirthQueue), queue:in(AddToBirthQueue + AddToGrowQueue, UpdatedGrowQueue), Days - 1).
+    iterate_day(queue:in(AddToBirthQueue, UpdatedBirthQueue), queue:in(AddToGrowQueue + AddToBirthQueue, UpdatedGrowQueue), Days - 1).
 
 
-merge_queues({{value, Val}, BirthQueue}, GrowQueue) ->
-    merge_queues(queue:out(BirthQueue), queue:in(Val, GrowQueue));
-merge_queues({empty, _BirthQueue}, GrowQueue) ->
+merege_queues({{value, Val}, BirthQueue}, GrowQueue) ->
+    merege_queues(queue:out(BirthQueue), queue:in(Val, GrowQueue));
+merege_queues({empty, _BirthQueue}, GrowQueue) ->
     GrowQueue.
